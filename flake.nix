52264cd807926c54cc70b85aca958e15760f9dde
Ryan <ryanricher@gmail.com>
2024-02-28 01:44:43 -0800

    added home manager initial

--- flake.nix
+++ flake.nix
@@ -3,16 +3,26 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;
+      system = "x86_64-linux"; 
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
         ryanpc = lib.nixosSystem {
-          system = "x86_64-linux";
+          inherit system;
           modules = [ ./configuration.nix ];
+        };
+      };
+      homeConfigurations = {
+        ryan = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
         };
       };
     };
