53efb9e83fc1325abe69236b42279df35f2847e2
rricher <ryanricher@gmail.com>
2024-02-29 17:28:05 -0800

    changed shell to zsh

--- profiles/desktop/configuration.nix
+++ profiles/desktop/configuration.nix
@@ -72,9 +72,9 @@
   ];
 
   # I use zsh btw
-  # environment.shells = with pkgs; [ zsh ];
-  # users.defaultUserShell = pkgs.zsh;
-  # programs.zsh.enable = true;
+  environment.shells = with pkgs; [ zsh ];
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
